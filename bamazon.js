b6e065045e104c4fd835dd0c18e7730f358d0b79
Kate Davis <katedavis@Kates-MacBook-Pro.local>
2020-02-22 11:51:05 -0600

    packages added

--- bamazon.js
+++ bamazon.js
@@ -1,8 +1,9 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+
 var connection = mysql.createConnection({
-    host: "localhost"
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
--- schema.sql
+++ schema.sql
@@ -1,12 +1,25 @@
 create database if not exists bamazon;
 use bamazon;
 
-create table products(
-	itemid integer auto_increment not null,
-    productname varchar(45) not null,
-    departmentname varchar(45) not null,
-    price decimal(10,4) not null,
-    stockquantity integer(10) not null,
-    primary key (itemid)
-    );
+CREATE TABLE products(
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT(10) NOT NULL,
+  primary key(item_id)
+);
     
+    SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Uncharted 4", "Video Games", 49.95, 150),
+  ("DOOM", "Video Games", 59.99, 200),
+  ("Crate of Spam", "Food and Drink", 24.50, 50),
+  ("Cool Shades", "Apparel", 75.00, 5),
+  ("Worn Denim Jeans", "Apparel", 54.25, 35),
+  ("Survival Towel", "Necessities", 42.42, 42),
+  ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
+  ("Mad Max: Fury Road", "Films", 25.50, 57),
+  ("Monopoly", "Board Games", 30.50, 35),
+  ("Yahtzee", "Board Games", 19.95, 23);
